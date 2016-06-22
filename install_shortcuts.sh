5adc222846a195405c3d3d15cee2e9b87b988d62
Dmitra <adrian@dmitra.com>
2016-06-22 12:22:37 +0300

    temporary solution for vertical window resize

--- install_shortcuts.sh
+++ install_shortcuts.sh
@@ -1,14 +1,24 @@
+  # Increase window horizontally
 gsettings set org.compiz.integrated command-1 "node ~/.window-manager/cli/resize+.js"
-gsettings set org.compiz.integrated run-command-1 "['<Super>KP_Add']"
+gsettings set org.compiz.integrated run-command-1 "['<Super>equal']"
+  # Decrease window horizontally
 gsettings set org.compiz.integrated command-2 "node ~/.window-manager/cli/resize-.js"
-gsettings set org.compiz.integrated run-command-2 "['<Super>KP_Subtract']"
-gsettings set org.compiz.integrated command-3 "node ~/.window-manager/cli/snapLeft.js"
-gsettings set org.compiz.integrated run-command-3 "['<Super>Left']"
-gsettings set org.compiz.integrated command-4 "node ~/.window-manager/cli/snapRight.js"
-gsettings set org.compiz.integrated run-command-4 "['<Super>Right']"
+gsettings set org.compiz.integrated run-command-2 "['<Super>minus']"
+  # Increase window vertically
+gsettings set org.compiz.integrated command-3 "wmctrl -r :ACTIVE: -b add,maximized_vert"
+gsettings set org.compiz.integrated run-command-3 "['<Shift><Super>plus']"
+  # Decrease window vertically
+gsettings set org.compiz.integrated command-4 "wmctrl -r :ACTIVE: -b remove,maximized_vert"
+gsettings set org.compiz.integrated run-command-4 "['<Shift><Super>underscore']"
+  # Snap window to left
+gsettings set org.compiz.integrated command-5 "node ~/.window-manager/cli/snapLeft.js"
+gsettings set org.compiz.integrated run-command-5 "['<Alt><Super>h']"
+  # Snap window to right
+gsettings set org.compiz.integrated command-6 "node ~/.window-manager/cli/snapRight.js"
+gsettings set org.compiz.integrated run-command-6 "['<Alt><Super>l']"
   # Snap window to top
-gsettings set org.compiz.integrated command-5 "xdotool getactivewindow windowmove x 0"
-gsettings set org.compiz.integrated run-command-5 "['<Super>Up']"
+gsettings set org.compiz.integrated command-7 "xdotool getactivewindow windowmove x 0"
+gsettings set org.compiz.integrated run-command-7 "['<Alt><Super>k']"
   # Snap window to bottom
-gsettings set org.compiz.integrated command-6 "xdotool getactivewindow windowmove x 999"
-gsettings set org.compiz.integrated run-command-6 "['<Super>Down']"
+gsettings set org.compiz.integrated command-8 "xdotool getactivewindow windowmove x 999"
+gsettings set org.compiz.integrated run-command-8 "['<Alt><Super>j']"
--- snapCurrentWindow.js
+++ snapCurrentWindow.js
@@ -9,6 +9,5 @@ module.exports = function (direction) {
 
   if (direction === 'Left') x = 0//screen.width - desktop.width
   if (direction === 'Right') x = screen.width - window.width
-    console.log(x)
   Util.run(`xdotool windowmove ${window.id} ${x} ${y}`)
 }
