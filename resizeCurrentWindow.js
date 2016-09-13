ba3d54dce7dce2051f3d59643a9ac0bc844a9528
Dmitra <adrian@dmitra.com>
2016-09-13 10:08:13 +0300

    add small sizes

--- resizeCurrentWindow.js
+++ resizeCurrentWindow.js
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
-Util = require('./util')
-var snapper = require('./snapCurrentWindow')
+const Util = require('./util')
+const snapper = require('./snapCurrentWindow')
 
 module.exports = function (direction) {
-  var window = Util.getActiveWindow()
-  , screen = Util.getScreenSize()
-  , desktop = Util.getDesktopSize()
-  , screenWidths = [768, 1024, 1280, 1600, 1920, 2500, 2560, 3440]
+  const window = Util.getActiveWindow()
+  const desktop = Util.getDesktopSize()
+  const screenWidths = [360, 480, 640, 768, 1024, 1280, 1600, 1920, 2500, 2560, 3440]
 
-  var widths = Util.limitScale(screenWidths, desktop.width)
+  const widths = Util.limitScale(screenWidths, desktop.width)
 
-  //var snapToRight = desktop.width === window.width + window.x
-  var snapToRight = Util.run(`xwininfo -id ${window.id} | grep "Corners" | awk '{print $3}'`).substring(0,2) === '-0'
+  // var snapToRight = desktop.width === window.width + window.x
+  const wininfo = Util.run(`xwininfo -id ${window.id} | grep "Corners" | awk '{print $3}'`)
+  const snapToRight = wininfo.substring(0, 2) === '-0'
   window.width = Util.step(window.width, direction, widths)
 
   Util.run(`xdotool windowsize ${window.id} ${window.width} ${window.height}`)
   // don't work ???
-  //Util.run(`winctrl -ri ${window.id} -e 0,0,0,${window.width},${window.height}`)
+  // Util.run(`winctrl -ri ${window.id} -e 0,0,0,${window.width},${window.height}`)
   if (snapToRight) snapper('Right')
 }
--- snapCurrentWindow.js
+++ snapCurrentWindow.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-Util = require('./util')
+const Util = require('./util')
 
 module.exports = function (direction) {
-  var window = Util.getActiveWindow()
-  , screen = Util.getScreenSize()
-  , x = 'x'
-  , y = 'y'
+  const window = Util.getActiveWindow()
+  const screen = Util.getScreenSize()
+  let x = 'x'
+  const y = 'y'
 
-  if (direction === 'Left') x = 0//screen.width - desktop.width
+  if (direction === 'Left') x = 0 // screen.width - desktop.width
   if (direction === 'Right') x = screen.width - window.width
   Util.run(`xdotool windowmove ${window.id} ${x} ${y}`)
 }
